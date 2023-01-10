71a50f26f58256f400cda6ad7d4c6e0be53f1289
J <118442144+model-map@users.noreply.github.com>
2023-01-10 14:24:03 +0530

    Add top/bottom split. Next: Maximise width.

--- extension.js
+++ extension.js
@@ -33,7 +33,7 @@ function getRectangles(window) {
 	return {
 		window: {
 			h: rect.height,
-			w: rect.width,
+			w: monitorWorkArea.width,
 			x: rect.x,
 			y: rect.y,
 		},
@@ -47,21 +47,14 @@ function getRectangles(window) {
 }
 
 function getResizeVal(workspaceAxis, windowAxis, c) {
-	const size1 = workspaceAxis / 3;
+	const size1 = workspaceAxis;
 	const size2 = workspaceAxis / 2;
-	const size3 = size1 * 2;
 
 	if (windowAxis < size1 - c) {
 		return size1;
 	}
-	if (windowAxis < size2 - c) {
-		return size2;
-	}
-	if (windowAxis < size3 - c) {
-		return size3;
-	}
 
-	return size1;
+	return size2;
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -73,46 +66,6 @@ function enable() {
 	const mode = Shell.ActionMode.NORMAL;
 	const flag = Meta.KeyBindingFlags.IGNORE_AUTOREPEAT;
 	const settings = getSettings();
-
-	Main.wm.addKeybinding('midscreen', settings, flag, mode, () => {
-		const window = getActiveWindow();
-		const rects = getRectangles(window);
-		const fourthWidth = rects.workspace.w / 4;
-		const fourthHeight = rects.workspace.h / 4;
-		const xStart = rects.workspace.x + (rects.workspace.w - (rects.workspace.w - fourthWidth)) / 2;
-		const yStart = (rects.workspace.h - (rects.workspace.h - fourthHeight)) / 2;
-		const newWidth = rects.workspace.w - fourthWidth;
-		const newHeight = rects.workspace.h - fourthHeight;
-
-		window.unmaximize(Meta.MaximizeFlags.BOTH);
-		window.move_frame(false, xStart, yStart);
-		window.move_resize_frame(false, xStart, yStart, newWidth, newHeight);
-	});
-
-	Main.wm.addKeybinding('toggle-left', settings, flag, mode, () => {
-		const window = getActiveWindow();
-		const rects = getRectangles(window);
-		const newWidth = getResizeVal(rects.workspace.w, rects.window.w, WidthDelta);
-
-		window.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
-		window.unmaximize(Meta.MaximizeFlags.VERTICAL);
-		window.move_frame(false, rects.workspace.x, 0);
-		window.move_resize_frame(false, rects.workspace.x, 0, newWidth, rects.window.h);
-		window.maximize(Meta.MaximizeFlags.VERTICAL);
-	});
-
-	Main.wm.addKeybinding('toggle-right', settings, flag, mode, () => {
-		const window = getActiveWindow();
-		const rects = getRectangles(window);
-		const newWidth = getResizeVal(rects.workspace.w, rects.window.w, WidthDelta);
-		const xStart = rects.workspace.x + rects.workspace.w - newWidth;
-
-		window.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
-		window.unmaximize(Meta.MaximizeFlags.VERTICAL);
-		window.move_frame(false, xStart, 0);
-		window.move_resize_frame(false, xStart, 0, newWidth, rects.window.h);
-		window.maximize(Meta.MaximizeFlags.VERTICAL);
-	});
 
 	Main.wm.addKeybinding('toggle-top', settings, flag, mode, () => {
 		const window = getActiveWindow();
